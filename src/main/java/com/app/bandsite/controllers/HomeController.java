962fb3ee20b1512827475f92eb19d042eab60341
Asen Georgiev <asen.georgiev@elando.com>
2023-11-07 18:44:02 +0200

    Deployment fixes

--- src/main/java/com/app/bandsite/controllers/HomeController.java
+++ src/main/java/com/app/bandsite/controllers/HomeController.java
@@ -1,8 +1,11 @@
 package com.app.bandsite.controllers;
 
 import com.app.bandsite.model.dtos.EventDto;
+import com.app.bandsite.repositories.EventRepository;
+import com.app.bandsite.repositories.UserRepository;
 import com.app.bandsite.services.EventService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,7 +18,7 @@ public class HomeController {
   private final EventService eventService;
 
   @Autowired
-  public HomeController(EventService eventService) {
+  public HomeController(EventService eventService, UserRepository userRepository, EventRepository eventRepository, PasswordEncoder passwordEncoder) {
     this.eventService = eventService;
   }
 
